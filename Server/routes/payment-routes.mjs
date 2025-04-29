74bbf82086da426006ee9c73a93329c65ec2b47a
VisarutTH <visarut.thk@gmail.com>
2025-04-29 22:25:13 +0700

    fix:feature payment

--- Server/routes/payment-routes.mjs
+++ Server/routes/payment-routes.mjs
@@ -51,20 +51,4 @@ payment.get("/payment-status/:paymentIntentId", async (req, res) => {
   }
 });
 
-payment.get("/customer-payment-methods/:customerId", async (req, res) => {
-  const { customerId } = req.params;
-
-  try {
-    const paymentMethods = await stripe.paymentMethods.list({
-      customer: customerId,
-      type: "promptpay",
-    });
-
-    res.status(200).json(paymentMethods);
-  } catch (error) {
-    console.error("Error fetching payment methods:", error);
-    res.status(500).json({ error: error.message });
-  }
-});
-
   export default payment
